97030ad8b8b57bc366b8fd6486e2f34732b997f3
codeUP <labesherwin@gmail.com>
2025-12-25 00:42:07 +0800

    initial front end

--- static/script.js
+++ static/script.js
@@ -1,8 +1,2 @@
-const params = new URLSearchParams(window.location.search);
-const role = params.get("role");
-
-const signupLink = document.getElementById("signup-link");
-
-if (signupLink && role !== "admin") {
-    signupLink.style.display = "none";
-}
+// For future interactivity (empty for now)
+console.log("Frontend initialized...");
